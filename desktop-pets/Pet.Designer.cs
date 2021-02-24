93cc3bb10f84232463be67d8b83fcdcececacec2
Jessica Wei <jessicawei9@gmail.com>
2021-02-24 13:37:13 -0800

    We have a cat that moves right

---
+++ desktop-pets/Pet.Designer.cs
@@ -0,0 +1,72 @@
+﻿
+namespace desktop_pets
+{
+    partial class Pet
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(99, 26);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ContextMenuStrip = this.contextMenuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.mousedown);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.mouseup);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ desktop-pets/Pet.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace desktop_pets
+{
+    public partial class Pet : Form
+    {
+        private int _DisplayHeight = SystemInformation.WorkingArea.Height;
+        private int _DisplayWidth = SystemInformation.WorkingArea.Width;
+        private Bitmap layer1 = new Bitmap("Art/Cat/idle.png");
+        private Bitmap layer2 = new Bitmap("Art/Cat/walk.png");
+
+        // Art Fields
+        // Walking
+        private Bitmap walk_sheet = new Bitmap("Art/Cat/walk_anim.png");
+        private List<Bitmap> walk = new List<Bitmap>();
+        private int walkInd = 0;
+        private bool walkVarient = false;
+
+
+        Timer timer = new Timer();
+        private int count = 0;
+
+        private int XSIZE = 64;
+        private int YSIZE = 64;
+        private int SINKLEVEL = 3;
+
+        private int HeadPoint = 0;
+
+        // Mode bools
+        private bool isDragging = false;
+
+        public Pet()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            // Load in Spritesheets
+            // Walk sheet
+            int col = walk_sheet.Width / XSIZE;
+            for (int i = 0; i < col; i++) {
+                Rectangle cloneRect = new Rectangle(XSIZE * i, 0, XSIZE, YSIZE);
+                walk.Add(walk_sheet.Clone(cloneRect, walk_sheet.PixelFormat));
+            }
+            
+            this.ShowInTaskbar = true;
+            this.TopMost = true;
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.Size = new Size(XSIZE, YSIZE);
+            Color transColor = layer1.GetPixel(0, 0);
+            layer1.MakeTransparent(transColor);
+            this.BackgroundImage = layer1;
+            this.BackColor = transColor;
+            this.TransparencyKey = transColor;
+            HeadPoint =_DisplayHeight - YSIZE + SINKLEVEL;
+            this.Location = new Point(0, HeadPoint);
+            timer.Interval = 1;
+            timer.Tick += new EventHandler(Timer_Tick);
+            timer.Start();
+        }
+        private void Timer_Tick(object sender, EventArgs e) {
+            if (this.Location.Y >= HeadPoint)
+            {
+                if (!isDragging)
+                {
+                    /*if (is_fly)
+                    {
+                        is_fly = false;
+                        fc1 = 0;
+                        fc2 = 0;
+                    }*/
+                    moveright();
+                }
+                else
+                {
+                   //dragmode();
+                }
+            }
+            else
+            {
+                if (!isDragging)
+                {
+                    //flymode();
+                }
+                else
+                {
+                    //dragmode();
+                }
+            }
+        }
+        
+        private void moveright() {
+            this.Left += 1;
+            if (this.Left > _DisplayWidth) {
+                this.Left = XSIZE *-1;
+                count = 0;
+                // Randomly swap to blinking & walking
+            }
+            if(!walkVarient)
+                GoThroughFrames(ref walk, ref walkInd, 10);
+            // else go through blink walk varient
+            
+        }
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void mousedown(object sender, MouseEventArgs e)
+        {
+            isDragging = true;
+        }
+
+        private void mouseup(object sender, MouseEventArgs e)
+        {
+            isDragging = false;
+        }
+        // use ref keyword to pass by reference
+        private void GoThroughFrames(ref List<Bitmap> bm, ref int index, int fps)
+        {
+            count++;
+            if (count % fps == 0) {
+                if (index >= bm.Count)
+                    index = 0;
+                this.BackgroundImage = bm[index];
+                index++;
+                count = 0;
+            }   
+        }
+    }
+}
